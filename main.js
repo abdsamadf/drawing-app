641e48a6b777d4ecdfb66d98026803bee3c87175
abdsamadf <abdulsamadf97@gmail.com>
2019-03-01 23:18:16 +0500

    improve freehand drawing with smoothing

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 const svg = d3.select('#svg');
-let drawing = false;
 
-function draw_point() {
-    if (!drawing)
-        return;
+let activeLine;
 
-    const coords = d3.mouse(this);
+const renderPath = d3.line()
+    .x(d => d[0])
+    .y(d => d[1])
+    .curve(d3.curveBasis)
 
-    const c = svg.append('circle')
-      .attr('cx', coords[0])
-      .attr('cy', coords[1])
-      .attr('r', 5)
-      .style('fill','black');
+
+svg.call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended));
+
+function dragstarted() {
+    activeLine = svg.append("path").datum([]).attr("class", "line").style('stroke', '#000').style('stroke-width', 3);
+    activeLine.datum().push(d3.mouse(this));
+    console.log('====================================');
+    console.log("start", activeLine.datum());
+    console.log('====================================');
 }
 
-svg.on('mousedown', () => {
-    drawing = true;
-});
+function dragged() {
+    activeLine.datum().push(d3.mouse(this));
+    activeLine.attr("d", renderPath);
+    console.log("dragged", activeLine.datum());
+}
 
-svg.on('mouseup', () => {
-    drawing = false;
-});
-svg.on('mousemove', draw_point);
+function dragended() {
+    activeLine = null;
+}
